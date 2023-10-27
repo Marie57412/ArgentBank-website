7ed9f6d4cad042a6cd4dd6f696f0464c4ce50c40
Marie57412 <mariebraun1@outlook.fr>
2023-10-27 13:23:15 +0200

    deplacement fichier

--- argent-bank/src/App.js
+++ argent-bank/src/App.js
@@ -1,12 +1,14 @@
-
+import React from 'react';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Header />
+     <Footer />
     </div>
   );
 }
