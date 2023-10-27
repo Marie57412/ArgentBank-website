d1e19703a188c19973be5389aede4fef295174fb
Marie57412 <mariebraun1@outlook.fr>
2023-10-27 11:18:16 +0200

    ajout footer

---
+++ argent-bank/src/Components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+    return (
+      <div className="footer">
+        <p className="footer-text">Copyright 2020 Argent Bank</p>
+      </div>
+    );
+  }
+  export default Footer;
