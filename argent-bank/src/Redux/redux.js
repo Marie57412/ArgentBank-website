3517ee2cd31a889a62deeee005c33901e4d367de
marie57412 <mariebraun1@outlook.fr>
2023-11-23 15:03:18 +0100

    ajout des commentaire

--- argent-bank/src/Redux/redux.js
+++ argent-bank/src/Redux/redux.js
@@ -1,27 +1,32 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
+// Création d'une tranche (slice) pour gérer les informations utilisateur dans Redux
 const userInfosSlice = createSlice({
-  name: 'user',
+  name: 'user', // Nom de la tranche (slice)
   initialState: {
-    token: null,
-    userName: localStorage.getItem('userName'),
-    firstName: localStorage.getItem('firstName'),
-    lastName: localStorage.getItem('lastName'),
+    token: null, // Token d'authentification de l'utilisateur
+    userName: localStorage.getItem('userName'), // Nom d'utilisateur récupéré du stockage local
+    firstName: localStorage.getItem('firstName'), // Prénom récupéré du stockage local
+    lastName: localStorage.getItem('lastName'), // Nom de famille récupéré du stockage local
   },
   reducers: {
+    // Action pour sauvegarder le token dans l'état Redux
     saveToken: (state, action) => {
       state.token = action.payload;
     },
+    // Action pour sauvegarder le nom d'utilisateur dans l'état Redux
     saveUserName: (state, action) => {
       state.userName = action.payload;
     },
+    // Action pour sauvegarder le prénom dans l'état Redux
     saveFirstName: (state, action) => {
       state.firstName = action.payload;
     },
+    // Action pour sauvegarder le nom de famille dans l'état Redux
     saveLastName: (state, action) => {
       state.lastName = action.payload;
     },
-
+    // Action pour réinitialiser les informations utilisateur dans l'état Redux
     resetUser: (state) => {
       return {
         token: null,
@@ -33,6 +38,7 @@ const userInfosSlice = createSlice({
   },
 });
 
+// Export des actions créées pour être utilisées dans d'autres parties du code
 export const {
   saveToken,
   saveUserName,
@@ -41,8 +47,9 @@ export const {
   resetUser,
 } = userInfosSlice.actions;
 
+// Configuration du store Redux en utilisant la tranche (slice) créée précédemment
 export const store = configureStore({
   reducer: {
-    user: userInfosSlice.reducer,
+    user: userInfosSlice.reducer, // Réducteur pour gérer les informations utilisateur
   },
 });
